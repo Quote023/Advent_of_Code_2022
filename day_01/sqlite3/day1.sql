67353ecda0ff84bdb289f702fe2e501dc8fcd23d
quote023 <acs2@ecomp.poli.br>
2022-12-02 09:12:52 -0300

    fix typo

--- day_01/sqlite3/day1.sql
+++ day_01/sqlite3/day1.sql
@@ -6,14 +6,14 @@ CREATE TEMP TABLE tmpElves (
 .import "../input.txt" tmpElves
 
 CREATE TEMP VIEW IF NOT EXISTS blank_lines AS SELECT ROWID as line FROM tmpElves where calories = '';
-CREATE TEMP VIEW IF NOT EXISTS limits AS SELECT LAG(line,1,0) OVER () as start,line as end FROM blank_line;
+CREATE TEMP VIEW IF NOT EXISTS limits AS SELECT LAG(line,1,0) OVER () as start,line as end FROM blank_lines;
 
 DROP TABLE IF EXISTS elves;
 CREATE TABLE elves AS
     SELECT limits.start as start, SUM(tmpElves.calories) as calories
     FROM limits
     JOIN tmpElves ON tmpElves.ROWID BETWEEN limits.start and limits.end
-    GROUP BY 1 ;
+    GROUP BY start;
 
 
 SELECT '1º', MAX(calories) FROM elves;
